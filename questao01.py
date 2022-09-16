3640c0c5534cffb366399657cb7f0e257eebf7ad
SarahRodrigues20 <113391554+SarahRodrigues20@users.noreply.github.com>
2022-09-16 11:27:18 -0300

    Add files via upload

---
+++ questao01.py
@@ -0,0 +1,9 @@
+pes:float ; jardas:float ; pol:float ; mil:float
+
+pes= float(input("informe a medida em pes: "))
+
+jardas= pes / 3
+pol= pes * 12
+mil= jardas / 760
+
+print(f" Sua medida em Jardas: {round (jardas,2)}), em Polegadas: {round (polegadas,2)}), em Milhas: {round (milhas,2)} ").
